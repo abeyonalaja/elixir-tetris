61f97dbf1c6f10bef9664ca7077d0bbad0defbbb
Abey Onalaja <Abey.onalaja@gmail.com>
2020-01-13 02:21:50 +0000

    add random name

---
+++ lib/brick.ex
@@ -0,0 +1,23 @@
+defmodule Tetris.Brick do
+  defstruct name: :i,
+            location: {40, 0},
+            rotation: 0,
+            reflection: false
+
+  def new(), do: __struct__()
+
+  def new_random() do
+    %{
+      name: random_name(),
+      location: {40, 0},
+      rotation: 0,
+      reflection: false
+    }
+  end
+
+  def random_name() do
+    ~w(i l z o t)a
+    |> Enum.shuffle()
+    |> List.first()
+  end
+end
