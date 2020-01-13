ab179d69ecf95695b632d709e6c681c3e446e6c2
Abey Onalaja <Abey.onalaja@gmail.com>
2020-01-13 14:27:50 +0000

    add tests for create random brick

--- lib/brick.ex
+++ lib/brick.ex
@@ -10,14 +10,23 @@ defmodule Tetris.Brick do
     %{
       name: random_name(),
       location: {40, 0},
-      rotation: 0,
-      reflection: false
+      rotation: random_rotation(),
+      reflection: random_reflection()
     }
   end
 
   def random_name() do
     ~w(i l z o t)a
-    |> Enum.shuffle()
-    |> List.first()
+    |> Enum.random()
+  end
+
+  def random_reflection() do
+    [true, false]
+    |> Enum.random()
+  end
+
+  def random_rotation() do
+    [0, 90, 180, 270]
+    |> Enum.random()
   end
 end
