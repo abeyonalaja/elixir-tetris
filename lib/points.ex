08dd3d6b8a580c5fce1757adeff679fe00580842
Abey Onalaja <Abey.onalaja@gmail.com>
2020-01-22 22:40:43 +0000

    transform and transpose points

--- lib/points.ex
+++ lib/points.ex
@@ -2,4 +2,35 @@ defmodule Tetris.Points do
   def translate(points, {x, y}) do
     Enum.map(points, fn {px, py} -> {px + x, py + y} end)
   end
+
+  def transpose(points) do
+    points
+    |> Enum.map(fn{x, y} -> {y, x} end)
+  end
+
+
+  def mirror(points) do
+    points
+    |> Enum.map(fn{x, y} -> {4-x, y} end)
+  end
+
+
+  def flip(points) do
+    points
+    |> Enum.map(fn{x, y} -> {x, 4-y} end)
+  end
+
+  def rotate_90(points) do
+    points
+    |> transpose
+    |> mirror
+  end
+
+  def rotate(points, 0), do: points
+  def rotate(points, degrees) do
+    rotate(
+    rotate_90(points), degrees - 90
+    )
+  end
+
 end
